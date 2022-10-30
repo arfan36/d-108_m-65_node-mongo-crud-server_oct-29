e4e97fb55807be68cc484242c8f7cceaa8436c36
arfan36 <arfanur36@gmail.com>
2022-10-30 15:46:01 +0600

    65-1 Module Overview, Async Await, Try Catch

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello from node mongo crud server');
 });
