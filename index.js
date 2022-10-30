4bb0017c6b1eac0ce4eb161b79a5c6b9d05b76ba
arfan36 <arfanur36@gmail.com>
2022-10-30 14:46:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from node mongo crud server');
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
