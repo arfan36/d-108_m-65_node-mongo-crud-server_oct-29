99f7eb1cd3b81923ffd8f3280ab6daee6baa4ad3
arfan36 <arfanur36@gmail.com>
2022-10-31 11:52:07 +0600

    65-4 Save Data From React Client Side To Mongo Database

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const userCollection = client.db('nodeMongoCrud').collection('users');
-        const user = {
-            name: 'dancing test',
-            email: 'danching@gmail.com'
-        };
-        const result = await userCollection.insertOne(user);
-        console.log(result);
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
     }
     finally {
 
